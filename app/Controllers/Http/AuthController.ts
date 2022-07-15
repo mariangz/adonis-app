b97208de694ee256e5a336797a3b26cbdb414fcf
Mariano Giménez <marianosgimenez@gmail.com>
2022-07-15 18:38:20 +0200

    feat: add signup validation

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,10 +1,26 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class AuthController {
-  public async signup({ request, response }: HttpContextContract) {
-    return response.json('Hello World')
+  public async signup({ request }: HttpContextContract) {
+    const signupSchema = schema.create({
+      name: schema.string(),
+      email: schema.string({}, [rules.email()]),
+      password: schema.string(),
+      confirmPassword: schema.string(),
+    })
+
+    const signupValidation = await request.validate({
+      schema: signupSchema,
+      messages: {
+        'name.required': 'Name is required',
+        'email.required': 'Email is required',
+        'password.required': 'Password is required',
+        'confirmPassword.required': 'Password Confirmation is required',
+      },
+    })
   }
-  public async login({ request, response }: HttpContextContract) {
-    return response.json('Hello World')
+  public async login({ request }: HttpContextContract) {
+    return request.all()
   }
 }
